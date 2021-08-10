e2fd8babcfde97f238cb0076ff3a9d1b0b69c197
Bhargavamacha <mbsai29@gmail.com>
2021-08-10 08:36:28 +0530

    delete

--- wg_napi/src/lib.rs
+++ wg_napi/src/lib.rs
@@ -1,13 +1,9 @@
 use boringtun::crypto::x25519;
-use boringtun::noise::Tunn;
 use core::str::FromStr;
 // use node_bindgen::derive::node_bindgen;
-use std::option::Option;
+use boringtun::noise::*;
 use std::sync::Arc;
 
-pub fn logger(log: String){
-    println!("{}", log);
-}
 // #[node_bindgen]
 pub fn wg_start() -> String {
     //   markdown_to_html(md.as_str(), &ComrakOptions::default())
@@ -15,19 +11,15 @@ pub fn wg_start() -> String {
         x25519::X25519SecretKey::from_str("AI7WqmJ+7pOuzORYEvwycPZ9dx9aRX+Ce3T1vhzgmnY=").unwrap();
     let public =
         x25519::X25519PublicKey::from_str("pGJECXEPsSHApNkTxEp/F0W24C/zJ84lnvCVrmREnEg=").unwrap();
-    let wg = Tunn::new(
+    let mut peer = Tunn::new(
         Arc::new(private),
         Arc::new(public),
-        Option::default(),
-        Option::default(),
-        0,
-        Option::default()
-        // peer_static_public: Arc<X25519PublicKey>,
-        // preshared_key: Option<[u8; 32]>,
-        // persistent_keepalive: Option<u16>,
-        // index: u32,
-        // rate_limiter: Option<Arc<RateLimiter>>,
-    );
-    wg.unwrap().set_logger(Box::new(), boringtun::noise::Verbosity::Trace);
+        None,
+        None,
+        100,
+        None,
+    )
+    .unwrap();
+    peer.set_logger(Box::new(|e: &str| eprintln!("client: {}", e)), Verbosity::Trace);
     return "Done".to_string();
 }
